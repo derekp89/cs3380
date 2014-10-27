9a631f00a28ee06ebecc11bedb698d930566569f
derekp89 <derekp89@gmail.com>
2014-10-27 12:47:12 -0500

    Added user and spice table

--- database.sql
+++ database.sql
@@ -1,1 +1,16 @@
---Will add more later
+CREATE TABLE Spices(
+  name varchar(50),
+  descr text,
+  price decimal(13,2),
+  size varchar(10),
+  id serial PRIMARY KEY,
+  food varchar(255),
+  catagory varchar(50));
+  
+CREATE TABLE Users(
+  email varchar(50),
+  name varchar(50),
+  user_Id serial PRIMARY KEY);
+  
+
+  
