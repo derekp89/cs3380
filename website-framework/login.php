c865c52d9004382374281d7fe2177bbc89b766b0
dmpkb4 113816 <dmpkb4@babbage.cs.missouri.edu>
2014-11-20 21:46:32 -0600

    edit login page

--- website-framework/login.php
+++ website-framework/login.php
@@ -11,8 +11,91 @@
 
 
 </head>
-
+	<style>
+		body { padding-bottom: 70px; }
+		#body_wrapper{
+			width: 1000px;
+			padding: auto;
+			margin: auto;
+		}
+		/*#myCarousel{
+			width: 700px;
+			float:left;
+		}
+		#search-by-tabs{
+			display:inline;
+			float:left;
+			margin-left:10px;
+		}*/
+		.alpha-highlight{
+			background-color: rgb(216, 226, 244);
+		}
+		.scale-img{
+			width:1000px;
+		}
+	</style>
+	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css"/>
+	<script src="jquery-ui-1.11.2/external/jquery/jquery.js"></script>
+	<script src="jquery-ui-1.11.2/jquery-ui.min.js"></script>
+	<script src="dist/js/bootstrap.js"></script>
+	<script>
+		/* Will create two dynamic tabs */
+		$(function() {
+			$( "#search-by-tabs,#tabs-1" ).tabs();
+		});
+		
+		/* Will highlight the search-by tabs which the user clicked on */
+		$(function() {
+			$( "#search-by li" ).click(function(){
+				$(this).addClass("active");
+				$("li").not(this).removeClass("active");
+			});
+		});
+		
+		/* Will highlight the alphabet tab which the user clicked on */
+		$(function() {
+			$( "#tabs-1 li" ).click(function(){
+				$(this).addClass("alpha-highlight");
+				$("li").not(this).removeClass("alpha-highlight");
+			});
+		});
+	</script>
+</head>
 <body>
+	<!-- Top Navigation Bar -->
+	<nav class="navbar navbar-inverse" role="navigation">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">Home</a>
+	    </div>
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+			 <!-- Drop down menu for user to choose search by alphabet or by category -->
+  	        <li class="dropdown">
+  	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Shop For Spices <span class="caret"></span></a>
+  	          <ul class="dropdown-menu" role="menu">
+  	            <li><a href="#">By Alphabet</a></li>
+				<li class="divider"></li>
+  	            <li><a href="#">By Category</a></li>
+  	          </ul>
+  	        </li>
+	        <li><a href="#">View Cart</a></li>
+	      </ul>
+	      <ul class="nav navbar-nav navbar-right">
+			<!-- Redirect to About Us page -->
+	        <li><a href="#">About Us</a></li>
+			<!-- Redirect to Login page-->
+	        <li><a href="http://babbage.cs.missouri.edu/~cs3380f14grp13/cs3380/website-framework/login.php">Log Into Your Account</a></li>
+	      </ul>
+	      <form class="navbar-form navbar-right" role="search">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Enter Search Term">
+	        </div>
+	        <button type="submit" class="btn btn-default">Search</button>
+	      </form>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
     <div class="container">
         <div class="row">
             <div class="col-sm-6 col-md-4 col-md-offset-4">
@@ -77,8 +160,14 @@
   
 ?>	
 	
+<!-- Bottom Navigation Bar -->
+	<nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+	  <div class="container">
+	      <ul class="nav navbar-nav navbar-right">
+			<!-- Redirect to About Us page -->
+	        <li><a href="http://babbage.cs.missouri.edu/~cs3380f14grp13/cs3380/website-framework/logout.php">Logout</a></li>
+		</ul>
+	  </div>
+	</nav>
 </body>
-
-
-
 </html>
