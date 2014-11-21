b0bb9e172350f8a205cb6dfd960a7c59238f931f
dmpkb4 113816 <dmpkb4@babbage.cs.missouri.edu>
2014-11-21 02:46:44 -0600

    removed unesserary header

--- website-framework/search_by_alphabet_handler.php
+++ website-framework/search_by_alphabet_handler.php
@@ -13,10 +13,6 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-</head>
 <body>
 	<table class="table table-hover">
 		<?php
