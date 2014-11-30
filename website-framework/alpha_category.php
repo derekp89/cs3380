62b9b30cb420dad00740aa0248186f57cad1c574
arhitchens <arhitchens@gmail.com>
2014-11-30 15:42:57 -0600

    Update alpha_category.php

--- website-framework/alpha_category.php
+++ website-framework/alpha_category.php
@@ -189,7 +189,7 @@
 			<li><a href="account.php"><?php echo ucfirst($_SESSION['username']); ?>'s Account</a></li>
 			<?php } ?>
 			<!-- Redirect to About Us page -->
-	        <li><a href="#">About Us</a></li>
+	        <li><a href="about_us.php">About Us</a></li>
 			<!-- Redirect to Login page-->
 	        <li><a href= <?=$href_page?> ><?=$log_display ?></a></li>
 	      </ul>
