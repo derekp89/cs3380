db024b7c8f1acbaac56cfca98178da67a21400d2
cs3380f14grp13 <cs3380f14grp13@babbage.cs.missouri.edu>
2014-11-30 15:22:49 -0600

    added recent changes

--- website-framework/changepass.php
+++ website-framework/changepass.php
@@ -74,11 +74,14 @@ function updatePass($npass,$id){
 		pg_execute($dbconn, "update",array($password, $uid));
 	}
 	
-function checkConfirm($npass,$confirm) {			
+function checkConfirm($npass,$confirm) {
+			
 	if($npass === $confirm) 
 		return 1;
 	else
 		return 0;
+	
+	
 }
 
 ?>
