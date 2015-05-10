9e91cf391637351cde534b2ee9b1df201247131d
dmpkb4 113816 <dmpkb4@babbage.cs.missouri.edu>
2015-05-10 16:32:06 -0500

    added nav to changepass

--- website-framework/changepass.php
+++ website-framework/changepass.php
@@ -108,48 +108,11 @@ function checkConfirm($npass,$confirm) {
 	<script src="jquery-ui-1.11.2/jquery-ui.min.js"></script>
 	<script src="dist/js/bootstrap.js"></script>
 	<link rel="stylesheet" href="css/sidebar.css">
-	</style>
+
 
 </head>
 <body>
-<!-- Top Navigation Bar -->
-	<nav class="navbar navbar-inverse" role="navigation">
-	  <div class="container-fluid">
-	    <div class="navbar-header">
-	      <a class="navbar-brand" href="home.php">Home</a>
-	    </div>
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav">
-			 <!-- Drop down menu for user to choose search by alphabet or by category -->
-  	        <li class="dropdown">
-  	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Shop For Spices <span class="caret"></span></a>
-  	          <ul class="dropdown-menu" role="menu">
-  	            <li><a href="alpha_category.php">By Alphabet</a></li>
-				<li class="divider"></li>
-  	            <li><a href="alpha_category.php#">By Category</a></li>
-  	          </ul>
-  	        </li>
-	        <li><a href="cart.php">View Cart</a></li>
-	      </ul>
-	      <ul class="nav navbar-nav navbar-right">
-		  <!-- Redirect to User account page -->
-		  <?php  if($_SESSION['username']){?>
-			<li><a href="account.php"><?php echo ucfirst($_SESSION['username']); ?>'s Account</a></li>
-			<?php } ?>
-			<!-- Redirect to About Us page -->
-	        <li><a href="about_us.php">About Us</a></li>
-			<!-- Redirect to Login page-->
-	        <li><a href= <?=$href_page?> ><?=$log_display ?></a></li>
-	      </ul>
-	      <form class="navbar-form navbar-right" action="search.php" method="post">
-	        <div class="form-group">
-	          <input type="text" class="form-control" name="search" placeholder="Enter Search Term" required>
-	        </div>
-	        <button type="submit" class="btn btn-default">Search</button>
-	      </form>
-	    </div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
+<?php include('nav.php'); ?>
 
 <div class="container-fluid">
       <div class="row">
